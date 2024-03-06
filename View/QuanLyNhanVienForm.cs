47aa4593e6e62700c22a90fe16c185a4a9c82576
Phạm Văn Quyết <quyet05122002@gmail.com>
2024-03-06 07:56:47 +0700

    Quyet set up

---
+++ View/QuanLyNhanVienForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectXML.View
+{
+    public partial class QuanLyNhanVienForm : Form
+    {
+        public QuanLyNhanVienForm()
+        {
+            InitializeComponent();
+        }
+
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new QuanLyNhanVienForm());
+        }
+    }
+}
